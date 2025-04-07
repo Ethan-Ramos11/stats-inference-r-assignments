591f3ca5f40eb5e46fea133cd009348a3ad45688
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-07 13:40:47 -0400

    Added assignment 3 (in progress)

---
+++ Assignment-3-Ramos.R
@@ -0,0 +1,33 @@
+#Chapter 3, Problems: 1, 5, 8, 9, 16, 18, 23,  25, 30. 
+
+#Chapter 5, Problems: 11, 13, 34
+
+#---------------
+#Answers: 
+#Problem 1a: Valid probability mass function 
+#Problem 1b: 
+#Problem 1c:
+#Problem 1d:
+
+#Problem 5: 1.125
+#---------------
+
+#1a - Valid probability mass function
+# (1/4) + (1/2) + (1/8) + (1/8) = 1
+# All probabilities are positive and they sum to 1 so it is a valid probabillity mass function
+
+#1b - 
+
+#1c - 
+
+#1d - 
+
+#5 - 1.125
+#To compute the mean of random variable X you multiply 
+#p(x) * x and sum them together
+#Stored the answer in m and printed it out
+
+m <- ((1/4) * 0) + (.5 * 1) + ((1/8) * 2) + ((1/8) * 3)
+m 
+
+#8
